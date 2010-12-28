a95e291b3710e6166dda7c885d40557b7778bdb4
unknown <Administrator@.(none)>
2010-12-28 15:43:38 +0530

    * Change get_max_message_id to update_max_message_id     * Change YUser.message_id to YUser.max_message_id     * The code inside the for loop at line 11-17 to be a method of YUser called: update_messages     * To prevent repetition of message_id, make message_id a primary_key. (Use try/catch to prevent repetitions)     * Fix the to_user field when saving a message     * Let mobile_no max_length=13...     * make Message.message max_length=140.     * Change Messages to Message.     * Change name of app from yammersmsindia to ysms     * Move SmsGupshupSender::username and password to settings.py & local_settings.py     * Remove access token from settings.py     * Add “local_settings.py” and read that from settings.py. Move yammer consumer key, secret, and SmsGupshup username, password to local_settings.py. settings.py should have None for those variables.
        * Fix ‘sms_messages’ view
        * Add ‘clear_messages’ view:
              o It should delete all Message objects with sms_sent older than one week
        * Create a new model called SentMessage
              o Should have YUser
              o Message
              o received_time
              o sent_time
        * Add a ‘receive_sms’ view:
              o It will receive one sms from sms gupshup
              o Use the phone number to lookup the YUser
              o If phone_number not found, return an error message
              o Then create an appropriate SentMessage object, with sms_sent=None
              o Now try to send the message to Yammer. If success: update sent_time=now().
              o return “Your message has been received” message

--- models.py
+++ models.py
@@ -9,7 +9,9 @@ class YUserManager(models.Manager):
     def authenticate_users (self):
         for yuser in self.all():
             yuser.update_messages()
-  
+    
+    
+    
 class YUser(models.Model):
     yammer_user_id=models.BigIntegerField(default=0)
     update_max_message_id=models.BigIntegerField(default=0)
@@ -17,11 +19,10 @@ class YUser(models.Model):
     mobile_no=models.CharField(max_length=13)
     oauth_token=models.CharField(max_length=150)
     oauth_token_secret=models.CharField(max_length=150)
-    
-    
     def __unicode__(self):
         return self.fullname
     objects = YUserManager()
+    
     def update_messages(self):
         yammer = Yammer(consumer_key=settings.YAMMER_CONSUMER_KEY, 
                  consumer_secret=settings.YAMMER_CONSUMER_SECRET,
@@ -30,7 +31,12 @@ class YUser(models.Model):
         all_messages= yammer.messages.sent(newer_than=self.update_max_message_id)
         self.get_all_messages(all_messages)  
         self.get_max_message_id()
-            
+    def post_message(self,content):
+        yammer = Yammer(consumer_key=settings.YAMMER_CONSUMER_KEY, 
+                 consumer_secret=settings.YAMMER_CONSUMER_SECRET,
+                 oauth_token=self.oauth_token,
+                 oauth_token_secret=self.oauth_token_secret) 
+        yammer.messages.post(content)         
     def get_all_messages(self,all_messages):
         for message in all_messages:         
             yammer_mes=Message(from_user=self,to_user=None,message=message['body']['parsed'],thread_id=message['thread_id'],message_id=message['id'])
@@ -41,7 +47,11 @@ class YUser(models.Model):
         if q.get('message_id__max', 0):
             self.update_max_message_id = q['message_id__max']
             self.save()    
-                
+class MessageManager(models.Manager):   
+    def delete_messages(self,date):
+        del_messages=Message.objects.filter(sms_sent__lt=date).all() 
+        del_messages.delete()
+                       
 class Message(models.Model):
     thread_id=models.BigIntegerField(default=0)
     message_id=models.BigIntegerField(primary_key=True, default=0)
@@ -51,5 +61,12 @@ class Message(models.Model):
     sms_sent=models.DateTimeField(null=True,editable=False)
     def __unicode__(self):
         return self.message
-     
-     
+    objects = MessageManager()
+    
+class SentMessage(models.Model): 
+    yuser=models.ForeignKey(YUser, null=True, blank=True)
+    message=models.CharField(max_length=140, editable=True)  
+    received_time=models.DateTimeField(null=True,editable=False)
+    sent_time= models.DateTimeField(null=True,editable=False) 
+    def __unicode__(self):
+        return self.message 
--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
-from views import get_messages,sms_messages
+from views import get_messages,sms_messages,receive_sms,clear_messages
 urlpatterns = patterns('', 
                        url(r'^$', get_messages, name='get_messages'),
-                       url(r'^sms_messages', sms_messages, name='sms_messages')
-                       url(r'^receive_sms', receive_sms, name=''),
+                       url(r'^sms_messages', sms_messages, name='sms_messages'),
+                       url(r'^receive_sms', receive_sms, name='receive_sms'),
+                       url(r'^clear_messages', clear_messages, name='clear_messages'),
                        )
