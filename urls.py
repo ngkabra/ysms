3cbbc30ac9e8214e1c4ca22fde4e359a6cff2f7c
Wogma Testuser <wogmatest@healthvikings.com>
2010-12-29 16:07:14 +0000

    ysms updated

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
-from views import get_messages,sms_messages
+from views import get_messages,sms_messages,receive_sms
 urlpatterns = patterns('', 
                        url(r'^$', get_messages, name='get_messages'),
-                       url(r'^sms_messages', sms_messages, name='sms_messages')
+                       url(r'^sms_messages', sms_messages, name='sms_messages'),
                        url(r'^receive_sms', receive_sms, name=''),
                        )
